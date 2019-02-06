71a8c5e621c4451fd733393cfdd3d692ebcf55b3
joseph <joseph.gonzalez@upr.edu>
2019-02-06 12:55:02 -0500

    add mvn files

---
+++ src/test/java/com/joe/testStubs/TestStubsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joe.testStubs;
+
+
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TestStubsApplicationTests {
+
+
+
+}
+
